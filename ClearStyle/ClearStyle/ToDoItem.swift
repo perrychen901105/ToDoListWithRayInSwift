6ae8fe2a8cf4179173d469f3271188dd5dc3bbdb
Perry <chenzhipeng901105@gmail.com>
2015-02-05 17:07:49 +0800

    add tableview

--- ClearStyle/ClearStyle/ToDoItem.swift
+++ ClearStyle/ClearStyle/ToDoItem.swift
@@ -9,5 +9,15 @@
 import UIKit
 
 class ToDoItem: NSObject {
-   
+   // A text description of this item.
+    var text: String
+    
+    // A boolean value taht determines the completed state of this item
+    var completed: Bool
+    
+    // Returns a ToDoItem initialized with the given text and default completed value.
+    init(text: String) {
+        self.text = text
+        self.completed = false
+    }
 }
--- ClearStyle/ClearStyle/ViewController.swift
+++ ClearStyle/ClearStyle/ViewController.swift
@@ -11,10 +11,31 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-    
+    var toDoItems = [ToDoItem]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if toDoItems.count > 0 {
+            return
+        }
+        toDoItems.append(ToDoItem(text: "feed the cat"))
+        toDoItems.append(ToDoItem(text: "buy eggs"))
+        toDoItems.append(ToDoItem(text: "watch WWDC videos"))
+        toDoItems.append(ToDoItem(text: "rule the Web"))
+        toDoItems.append(ToDoItem(text: "buy a new iPhone"))
+        toDoItems.append(ToDoItem(text: "darn holes in socks"))
+        toDoItems.append(ToDoItem(text: "write this tutorial"))
+        toDoItems.append(ToDoItem(text: "master Swift"))
+        toDoItems.append(ToDoItem(text: "get more exercise"))
+        toDoItems.append(ToDoItem(text: "catch up with Mom"))
+        toDoItems.append(ToDoItem(text: "get a hair cut"))
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        tableView.separatorStyle = .None
+        tableView.rowHeight = 50.0
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -24,5 +45,26 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    // MARK: - Table view data source
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoItems.count
+    }
+
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
+        let item = toDoItems[indexPath.row]
+        cell.textLabel?.text = item.text
+        return cell
+    }
+    
+    
 }
 
