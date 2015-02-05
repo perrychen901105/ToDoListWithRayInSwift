b717d5cf150a08cf91b74c3da753f8399a775a97
Perry <chenzhipeng901105@gmail.com>
2015-02-05 17:29:22 +0800

    add table delegate methods

--- ClearStyle/ClearStyle/ViewController.swift
+++ ClearStyle/ClearStyle/ViewController.swift
@@ -56,7 +56,6 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return toDoItems.count
     }
-
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
@@ -65,6 +64,15 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         return cell
     }
     
+    // MARD: - Table view delegate
+    func colorForIndex(index: Int) -> UIColor {
+        let itemCount = toDoItems.count - 1
+        let val = (CGFloat(index) / CGFloat(itemCount)) * 0.6
+        return UIColor(red: 1.0, green: val, blue: 0.0, alpha: 1.0)
+    }
     
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        cell.backgroundView = colorForIndex(indexPath.row)
+    }
 }
 
