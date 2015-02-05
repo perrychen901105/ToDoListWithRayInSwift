4e80339d1c37d3458813ffe28c721aa969763c82
Perry <chenzhipeng901105@gmail.com>
2015-02-05 16:50:41 +0800

    add start project

---
+++ ClearStyle/ClearStyle/ToDoItem.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoItem.swift
+//  ClearStyle
+//
+//  Created by chenzhipeng on 15/2/5.
+//  Copyright (c) 2015年 chenzhipeng. All rights reserved.
+//
+
+import UIKit
+
+class ToDoItem: NSObject {
+   
+}
---
+++ ClearStyle/ClearStyle/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ClearStyle
+//
+//  Created by chenzhipeng on 15/2/5.
+//  Copyright (c) 2015年 chenzhipeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
